330b5dc1597f1ffe19a53fe4da56e1f2064fa793
Nuri Engin <nuri@engin.web.tr>
2021-11-27 14:25:37 +0300

    ✨ : The first  controller class generated for the Products and index/store functions have been enabled.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,18 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function(){
-    return Product::all();
-});
-
-Route::post('/products', function(){
-    return Product::create([
-        'name' => 'Product 1',
-        'slug' => 'product-one',
-        'description' => 'This is a product one',
-        'price' => '99.99'
-    ]);
-});
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::update('/products', [ProductController::class, 'update']);
+Route::delete('/products', [ProductController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
