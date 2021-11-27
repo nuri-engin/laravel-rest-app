0fd335a047f765f5ed0e3b541e07e6d41299c544
Nuri Engin <nuri@engin.web.tr>
2021-11-27 16:36:06 +0300

    ✨ : Protected routes introduced to avoid unauthenticated actions

--- routes/api.php
+++ routes/api.php
@@ -16,15 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('products', ProductController::class);
+// Public routes
+// Route::resource('products', ProductController::class);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('products/search/{name}', [ProductController::class, 'search']);
 
-// Enable those routes later on for the authentication purposes.
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::post('/products', [ProductController::class, 'store']);
-// Route::update('/products', [ProductController::class, 'update']);
-// Route::delete('/products', [ProductController::class, 'destroy']);
-
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
